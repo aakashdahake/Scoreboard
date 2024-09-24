3a86159be293c0b4c63f3a9dfb0e6c692acb112d
Aakash Dahake <aakashdahake@gmail.com>
2024-09-24 12:49:20 +0200

    Added en match logic and logger for logging

--- src/main/java/org/scoreboard/BusinessLogic/HandleScoreBoardImpl.java
+++ src/main/java/org/scoreboard/BusinessLogic/HandleScoreBoardImpl.java
@@ -1,5 +1,7 @@
 package org.scoreboard.BusinessLogic;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.scoreboard.Model.DTO.FootballMatch;
 import org.scoreboard.Model.DTO.Team;
 import org.scoreboard.Model.Error.ScoreboardException;
@@ -9,8 +11,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class HandleScoreBoardImpl implements HandleScoreBoard {
 
     private final ConcurrentHashMap<String, FootballMatch> matches = new ConcurrentHashMap<>();
-
-
+    private final static Logger LOGGER = LogManager.getLogger(HandleScoreBoardImpl.class);
     private boolean isTeamAlreadyPlaying(Team homeTeam, Team awayTeam) {
         for (FootballMatch match : matches.values()) {
             if (match.getMatchKey().contains(homeTeam.getName()) || match.getMatchKey().contains(awayTeam.getName())) {
@@ -31,6 +32,7 @@ public class HandleScoreBoardImpl implements HandleScoreBoard {
         if (isTeamAlreadyPlaying(newMatch.getHomeTeam(), newMatch.getAwayTeam())) {
             throw new ScoreboardException("Team already playing in another match");
         }
+        LOGGER.info("Starting new match :: " + matchName);
         matches.put(matchName, newMatch);
     }
 
@@ -52,22 +54,36 @@ public class HandleScoreBoardImpl implements HandleScoreBoard {
     @Override
     public void updateScore(FootballMatch currentMatch, int homeTeamScore, int awayTeamScore) {
 
+
         if (currentMatch == null || !matches.containsKey(currentMatch.getMatchKey())) {
             throw new ScoreboardException("Match not found");
         }
 
         validateScores(homeTeamScore, awayTeamScore, currentMatch);
 
+        LOGGER.info("Updating score for match :: " + currentMatch.getMatchKey());
+
         FootballMatch match = matches.get(currentMatch.getMatchKey());
         synchronized (match) {
+            LOGGER.info("Setting score for home team :: " + match.getHomeTeam().getName() + " :: " + homeTeamScore);
             match.getHomeTeam().setScore(homeTeamScore);
+            LOGGER.info("Setting score for away team :: " + match.getAwayTeam().getName() + " :: " + awayTeamScore);
             match.getAwayTeam().setScore(awayTeamScore);
         }
     }
 
     @Override
     public void endMatch(FootballMatch currentMatch) {
+        if (currentMatch == null || !matches.containsKey(currentMatch.getMatchKey())) {
+            throw new ScoreboardException("Match not found");
+        }
 
+        FootballMatch match = matches.get(currentMatch.getMatchKey());
+        if(match == null || !match.isMatchActive()) {
+            throw new ScoreboardException("Can not end match that is not active or does not exist");
+        }
+        LOGGER.info("Ending match :: " + match.getMatchKey());
+        match.setMatchActive(false);
     }
 
 }
