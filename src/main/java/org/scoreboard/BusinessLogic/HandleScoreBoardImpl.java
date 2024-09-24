f226f14836c9120c0ce3077a83eb286fe94a3737
Aakash Dahake <aakashdahake@gmail.com>
2024-09-24 10:54:55 +0200

    Added start match implementation

--- src/main/java/org/scoreboard/BusinessLogic/HandleScoreBoardImpl.java
+++ src/main/java/org/scoreboard/BusinessLogic/HandleScoreBoardImpl.java
@@ -1,6 +1,8 @@
 package org.scoreboard.BusinessLogic;
 
 import org.scoreboard.Model.DTO.FootballMatch;
+import org.scoreboard.Model.DTO.Team;
+import org.scoreboard.Model.Error.ScoreboardException;
 
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -9,9 +11,27 @@ public class HandleScoreBoardImpl implements HandleScoreBoard {
     private final ConcurrentHashMap<String, FootballMatch> matches = new ConcurrentHashMap<>();
 
 
+    private boolean isTeamAlreadyPlaying(Team homeTeam, Team awayTeam) {
+        for (FootballMatch match : matches.values()) {
+            if (match.getMatchKey().contains(homeTeam.getName()) || match.getMatchKey().contains(awayTeam.getName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public void startMatch(FootballMatch newMatch) {
+        String matchName = newMatch.getMatchKey();
 
+        if (matches.containsKey(matchName)) {
+            throw new ScoreboardException("Match already exists");
+        }
+
+        if (isTeamAlreadyPlaying(newMatch.getHomeTeam(), newMatch.getAwayTeam())) {
+            throw new ScoreboardException("Team already playing in another match");
+        }
+        matches.put(matchName, newMatch);
     }
 
     @Override
